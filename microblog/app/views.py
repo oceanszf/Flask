fe0954477331c7a21c1d56e118f362f981190541
oceanszf <jeffrey.su@suzf.net>
2016-01-10 16:52:20 +0800

    template list

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -10,4 +10,12 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'jeffrey'}
-    return render_template("index.html", title="Home", user=user)
+    posts = [{
+            'author': {'nickname': 'Jhon'},
+            'body': 'Beatiful day in Portland!'
+            },{
+            'author': {'nickname': 'Susan'},
+            'body': 'The avengers movie was so cool!'
+            }
+            ]
+    return render_template("index.html", title="Home", user=user,posts=posts)
