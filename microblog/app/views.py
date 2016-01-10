b09b2c7482704d0638fac12dffabf0eaa3e4cacd
oceanszf <jeffrey.su@suzf.net>
2016-01-10 16:33:27 +0800

    template

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'jeffrey'}
+    return render_template("index.html", title="Home", user=user)
